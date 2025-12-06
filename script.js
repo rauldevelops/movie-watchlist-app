3ee4e6ef8011d1defa78d1a86daa924e84a6b311
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2025-12-06 03:31:58 -0500

    Styled movie search result HTML and refactored API functions

--- script.js
+++ script.js
@@ -11,20 +11,29 @@ searchForm.addEventListener('submit', async (e) => {
             const movieDetails = await Promise.all(data.Search.map(getMovieDetails))
             movieDetails.forEach(movie => {
                 resultsHtml += `
-                        <div class="movie-card">
-                            <img src="${movie.Poster}" alt="${movie.Title} Poster" />
-                            <div class="movie-info">
-                                <div class="title-rating">
+                        <div class="movie-card flex" data-movie='${movie.imdbID}'>
+                            <img class="movie-poster" src="${movie.Poster}" alt="${movie.Title} Poster" />
+                            <div class="movie-info flex">
+                                <div class="title-rating flex">
                                     <h3>${movie.Title}</h3>
-                                    <p>⭐️${movie.imdbRating}</p>
+                                    <p>⭐️ ${movie.Rating.slice(0, -3)}</p>
                                 </div>
-                                <div class="runtime-genre-watchlist-btn"
-                                <p>${movie.Year}</p>
+                                <div class="runtime-genre-watchlist-btn flex"
+                                    <p>${movie.Runtime}</p>
+                                    <p>${movie.Genre}</p>
+                                    <button class="watchlist-btn" data-movie='${movie.imdbID}'><i class="plus-icon fa-solid fa-circle-plus" style="color: #ffffff;"></i> Watchlist</button>
+                                </div>
+                                <div class="plot">
+                                    <p>${movie.Plot}</p>
+                                </div>
                             </div>
                         </div>
                     `
             })
+            document.getElementById('results-section').innerHTML = resultsHtml
     })
+    searchForm.reset()
+})
 
 async function getMovieDetails(movie) {
     const response = await fetch(`http://www.omdbapi.com/?apikey=44d7feb2&i=${movie.imdbID}`)
